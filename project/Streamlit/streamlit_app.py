11b5e22590e6c2e828ee007340358139b265514c
Laura-gr <62286892+Laura-gr@users.noreply.github.com>
2024-01-25 17:16:47 +0100

    Updated streamlit layout
    Hid the parameters in an expanding box, Added the reference to the paper and a short description.

--- project/Streamlit/streamlit_app.py
+++ project/Streamlit/streamlit_app.py
@@ -14,20 +14,17 @@ import requests
 import json
 
 #### Title
-st.title('_Topological data analysis_ for crash detection \n based on insert ref of paper')
 
-st.markdown('Here we showcase different plots related to closing stock market values. These plots are based on INSERT REF and are computed using some _Topological Data Analysis_ tools.')
+st.title('_Topological data analysis for crash detection_ \n based on insert ref of paper')
+
+st.markdown('Here we showcase different plots related to closing stock market values. These plots are based on the paper [Topological data analysis of financial time series: Landscapes of crashes](https://www.sciencedirect.com/science/article/abs/pii/S0378437117309202) by _Marian Gidea_ and _Yuri Katz_ and are computed using some _Topological Data Analysis_ tools. \n Their idea is the following.')
+st.markdown('>We use persistence homology to detect and quantify topological patterns that appear in multidimensional time series. Using a sliding window, we extract time-dependent point cloud data sets, to which we associate a topological space. We detect transient loops that appear in this space, and we measure their persistence. This is encoded in real-valued functions referred to as a ’persistence landscapes’. We quantify the temporal changes in persistence landscapes via their $L^p$-norms.')
 ##### General input parameters
 
 st.header('Choosing the parameters')
 
-st.subheader('Time scope for the analysis')
+with st.expander('Parameters for the analysis. :small[Here you can choose the stock indexes you want to look at and some other technical parameters for the computations]'):
 
-
-tab1, tab2, tab3=st.tabs(['Date scope','TDA parametes', 'Stock parameters'])
-
-
-with tab1 :
     ### Date
     min_date = datetime.date(2020, 1, 1)
     max_date = datetime.date(2022, 12, 31)
@@ -36,15 +33,10 @@ with tab1 :
     start_date = st.date_input("Start date", min_value=min_date, max_value=max_date, value=min_date)
     end_date = st.date_input("End date", min_value=min_date, max_value=max_date, value=max_date)
 
-    if start_date <= end_date:
-        st.success(" Chosen start date: `{}`\n\n Chosen end date:`{}`".format(start_date, end_date))
-    else:
-        st.error("Error: End date must be after start date.")
-
+    
 
 ### TDA params to select
 
-with tab2 :
 
     st.subheader('TDA parameters for the analysis')
 
@@ -59,6 +51,7 @@ with tab2 :
     norm=st.selectbox('Please select the norm',(1,2,3))
     st.caption('_Here you are choosing $L^p$ norm will be used._')
 
+
     type_of_filter=st.selectbox('Please select the filter',('None','low','high'))
     st.caption('_Filters can be applied during the computations._')
 
@@ -69,17 +62,33 @@ with tab2 :
     size_computation_window=st.radio('Select the size of the computation window', (100,250,500))
 ### Stocks  to select
 
-with tab3 :
-     stock_name = st.selectbox('Please choose at least three stocks to analyze', ('AAPL','TSLA','AMZN','MSFT'))
+    stocks=st.multiselect('Choose a stock index', ['AAPL','TSLA','AMZN','MSFT'])
+    
+if len(stocks)<3:
+           st.error('You need to chose at least three stock indices')
+
+    
+if start_date <= end_date:
+        st.success(" Chosen start date: `{}`\n\n Chosen end date:`{}`.".format(start_date, end_date))
+else:
+        st.error("Error: End date must be after start date.")
+
+if type_of_filter=='None':
+       st.write('You chose to apply no filter to your data.')
+else :
+       st.write('You are considering a {} filter'.format(type_of_filter))
+
+st.write('Moreover, the parameters you chose are the following. You consider a rolling window of {size_window} days, an $L^{p}$ norm. The plot you are looking at is an {plot_chosen} with frequency cut ${freq_cut}$ \n You are looking at the {stocks_chosen}'.format(size_window=size_persistence_window, p=norm, plot_chosen=type_of_plot, freq_cut= 10**-cut_freq, stocks_chosen=stocks))
+
+for stock_name in stocks :
+    stock_data = yf.download(stock_name, start=start_date, end=end_date)
+    stock_data.reset_index(inplace=True)
 
 
-stock_data = yf.download(stock_name, start=start_date, end=end_date)
-stock_data.reset_index(inplace=True)
-
-fig = go.Figure()
-fig.add_trace(go.Scatter(x=stock_data.index, y=stock_data['Close'], name='Close'))
-fig.update_layout(title=f"{stock_name} Stock Price")
-fig.update_xaxes(title_text='Number of days')
-fig.update_yaxes(title_text='Close value')
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=stock_data.Date, y=stock_data['Close'], name='Close'))
+    fig.update_layout(title=f"{stock_name} Stock Price")
+    fig.update_xaxes(title_text='Date')
+    fig.update_yaxes(title_text='Close value')
 #fig.update_layout(y_label='Close value', x_label='Number of days')
-st.plotly_chart(fig)
+    st.plotly_chart(fig)
